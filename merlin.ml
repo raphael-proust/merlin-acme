daf5340a94f510845ca17c464282fbbad996cc79
Simon Castellan <simon.castellan@iuwt.fr>
2014-02-18 21:43:46 +0000

    Merlin interface: first version

---
+++ merlin.ml
@@ -0,0 +1,92 @@
+open Batteries
+module Json = Yojson.Basic
+type state = IO.input * unit IO.output
+
+let start command args = 
+  Unix.open_process ~autoclose: true ~cleanup: true 
+    (Printf.sprintf "%s %s"
+       command (String.concat " " (List.map Filename.quote args)))
+
+let close (input, output) =
+  Unix.close_process (input, output)
+
+type position = int * int
+type result = 
+| Return of Json.json
+| Error of string
+
+let string_of_json = function `String s -> s | _ -> assert false
+let int_of_json = function `Int n -> n | _ -> assert false
+let pos_of_json a = match a with
+  | `Assoc l ->
+    int_of_json (List.assoc "line" l), int_of_json (List.assoc "col" l)
+  | _ -> (-1, -1)
+
+let json_of_pos (a, b) = 
+  `Assoc ["line", `Int a; "col", `Int b]
+
+let json_of_range (start, stop) =
+  `Assoc ["start", json_of_pos start; 
+          "stop", json_of_pos stop]
+
+let range_of_json = function
+  | `Assoc l -> pos_of_json (List.assoc "start" l), pos_of_json (List.assoc "end" l)
+
+let send_command (input, output) command args = 
+  Printf.fprintf output "%s\n" (Json.to_string (`List (`String command :: args)));
+  Printf.fprintf stdout "%s\n" (Json.to_string (`List (`String command :: args)));
+  flush output;
+  let s = IO.read_line input in
+  print_endline s;
+  match Json.from_string s with
+  | `List ([`String "return"; data]) -> Return data
+  | `List ([`String "error"; `String s]) -> Error s
+  | `List ([`String "failure"; `String s]) -> Error s
+  | _ -> Error ("Unknown JSON")
+
+let send_command_map ~state command args cb = 
+  match send_command state command args with
+  | Return a -> (try Some (cb a) with _ -> None)
+  | Error s -> prerr_endline s; None
+let load_project ~state filename = 
+  send_command_map ~state "project" [`String "find"; `String filename] 
+    (function 
+    | `List l -> List.map string_of_json l | _ -> assert false)
+
+let reset ~state filename = 
+  send_command_map ~state "reset" [`String "name"; `String filename] (fun _ -> ())
+
+
+let tell_more ~state code = 
+  send_command_map ~state "tell" [`String "more"; `String code] 
+    (function `Null -> None
+    | `Assoc _ as data -> Some (pos_of_json data)
+    | _ -> assert false)
+
+let tell_source ~state code = 
+  send_command_map ~state "tell" [`String "source"; `String code] (fun _ -> ())
+let tell_end ~state = send_command_map ~state "tell" [`String "end"]
+    (function `Null -> None
+    | `Assoc _ as data -> Some (pos_of_json data)
+    | _ -> assert false)
+
+let tell_string ~state code = 
+  Option.bind (tell_source ~state code) (fun () ->
+    tell_end ~state)
+
+let type_enclosing ~state (string, offset) pos = 
+  send_command_map ~state "type" 
+    [`String "enclosing"; 
+     `Assoc ["expr", `String string; "offset", `Int offset]; 
+     json_of_pos pos]
+    (function `List l ->
+      List.map (function 
+      | `Assoc l as data -> (List.assoc "type" l, range_of_json data)
+      | _ -> assert false) l
+    | _ -> assert false)
+
+let type_expression ~state string pos = 
+  send_command_map ~state "type" 
+    [`String "expression"; `String string; `String "at"; json_of_pos pos]
+    (function `String s -> s
+    | _ -> assert false)
