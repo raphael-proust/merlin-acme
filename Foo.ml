7c876a7d9abfc97083075927694a28a18bf7caeb
Raphaël Proust <raphlalou@gmail.com>
2014-02-18 14:16:38 +0000

    modular

--- Foo.ml
+++ Foo.ml
@@ -1,13 +1,19 @@
 
-let ns = Sys.getenv "NAMESPACE"
-let wid = int_of_string (Sys.getenv "winid")
-let file = Sys.getenv "%"
-let user = Sys.getenv "USER"
+let env k = Sys.getenv k
 
-let () = O9pc.(
-		(*establish connection*)
-	let conn = connect (Printf.sprintf "%s/acme" ns) in
+(* Global env *)
+let ns = env "NAMESPACE"
+let user = env "USER"
 
+(* window specific env *)
+let gwid () = int_of_string (env "winid")
+let gfile () = env "%"
+
+(* global connection to the fs interface *)
+let conn = O9pc.connect (Printf.sprintf "%s/acme" ns)
+
+
+let new_window () = O9pc.(
 		(*get root attach, get the data and list the files*)
 	let root = attach conn user "" in
 	let root_iounit = fopen conn root oREAD in
@@ -21,25 +27,27 @@ let () = O9pc.(
 	let mwin_fid = walk conn root false "new/body" in
 	let mwin_iounit = fopen conn mwin_fid oWRITE in
 		(*this makes a new window*)
-	let (_:int32) = write conn mwin_fid mwin_iounit 0L 4l "foo\n" in
-		(*this writes to the same window*)
-	let (_:int32) = write conn mwin_fid mwin_iounit 0L 4l "bar\n" in
+	let (_:int32) = write conn mwin_fid mwin_iounit 0L 0l "" in
 		(*list files again*)
 	let root_iounit = fopen conn root oREAD in
 	let data = read conn root root_iounit 0L 4096l in
 	let newfiles = List.map (fun x -> x.Fcall.name) (unpack_files data) in
+	clunk conn mwin_fid;
+	clunk conn root;
 
 		(*get the new file*)
 	let filename (*racy*) = List.find (fun f -> not (List.exists (fun ff -> f = ff) files)) newfiles in
-	Printf.printf "%s\n%!" filename;
-
-		(*close the root attach because we can't "walk an open file"*)
-	clunk conn root;	
-	let root = attach conn user "" in
-	let mwin_fid = walk conn root false (Printf.sprintf "%s/body" filename) in
-	let mwin_iounit = fopen conn mwin_fid oWRITE in
-	
-		(*write in the new window*)
-	let (_:int32) = write conn mwin_fid mwin_iounit 0L 4l "baz\n" in
-	()
+	filename
 )
+
+let put winid s = O9pc.(
+	let root = attach conn user "" in
+	let win_fid = walk conn root false (Printf.sprintf "%s/body" winid) in
+	let win_iounit = fopen conn win_fid oWRITE in
+	let (_:int32) = write conn win_fid win_iounit 0L (Int32.of_int (String.length s)) s in
+	clunk conn win_fid
+)
+let win_name = new_window ()
+
+let () = put win_name "foo\nsplash"
+
