c17ba7139c21a7495453510fa306b4d60e1df089
Raphaël Proust <raphlalou@gmail.com>
2014-02-21 13:06:28 +0000

    use ocaml9p from opam
    (Pull request to make ocaml9p part of opam is open.)

--- LibAcme.ml
+++ LibAcme.ml
@@ -39,42 +39,42 @@ let string_of_ctl_msg = function
 	| Put -> "put"
 	| Show -> "show"
 
-let env k = Sys.getenv k
 
 (* Global env *)
-let ns = env "NAMESPACE"
-let user = env "USER"
+let ns = Sys.getenv "NAMESPACE"
+let user = Sys.getenv "USER"
+let reuse = ()
 
 (* window specific env *)
-let gwid () = env "winid"
-let gfile () = env "%"
+let gwid () = Sys.getenv "winid"
+let gfile () = Sys.getenv "%"
 
 let get_conn () = O9pc.connect (Printf.sprintf "%s/acme" ns)
 
 let get_fullio perm fname = O9pc.(
 	let conn = get_conn () in
-	let fid = attach conn user "" in
-	let fid = walk conn fid true fname in
+	let fid = attach conn ~user "" in
+	let fid = walk conn fid ~reuse fname in
 	let io = fopen conn fid perm in
 	(conn, fid, io)
 )
 let get_io conn perm fname = O9pc.(
-	let fid = attach conn user "" in
-	let fid = walk conn fid true fname in
+	let fid = attach conn ~user  "" in
+	let fid = walk conn fid ~reuse fname in
 	let io = fopen conn fid perm in
 	(fid, io)
 )
 
 let new_window () = O9pc.(
 	let (conn, fid, io) = get_fullio oREAD "" in
-	let data = read conn fid io 0L 4096l in
+	let data = read conn fid io 4096l in
 	let files = List.map (fun x -> x.Fcall.name) (unpack_files data) in
 	clunk conn fid;
 	let (fid, io) = get_io conn oWRITE "new/body" in
-	let (_:int32) = write conn fid io 0L 0l "" in
+	let (_:int32) = write conn fid io "" in
 	clunk conn fid;
 	let (fid, io) = get_io conn oREAD "" in
-	let data = read conn fid io 0L 4096l in
+	let data = read conn fid io 4096l in
 	let newfiles = List.map (fun x -> x.Fcall.name) (unpack_files data) in
 	clunk conn fid;
 	let filename (*racy*) = List.find (fun f -> not (List.exists (fun ff -> f = ff) files)) newfiles in
@@ -89,15 +89,15 @@ let merlin_winid =
 			close_in i;
 			(* test wether the file exists *)
 			let conn = get_conn () in
-			let fid = O9pc.attach conn user "" in
-			let fid = O9pc.walk conn fid true s in
+			let fid = O9pc.attach conn ~user  "" in
+			let fid = O9pc.walk conn fid ~reuse s in
 			O9pc.clunk conn fid;
 			s
 		with e -> begin
 			Printf.eprintf "%s\n%!" (Printexc.to_string e);
 			let mw = new_window () in
 			let (ctlconn, ctlfid, ctlio) = get_fullio O9pc.oWRITE (Printf.sprintf "%s/ctl" mw) in
-			let (_:int32) = O9pc.write ctlconn ctlfid ctlio 0L 13l "name +Merlin\n" in
+			let (_:int32) = O9pc.write ctlconn ctlfid ctlio "name +Merlin\n" in
 			let o = open_out_gen [Open_wronly; Open_creat; Open_trunc] 0o600 (Printf.sprintf "%s/mwinid" ns) in
 			output_string o mw;
 			flush o;
@@ -111,15 +111,15 @@ let erase_and_put ms =
 	let (addrconn, addrfid, addrio) = get_fullio O9pc.oWRITE (Printf.sprintf "%s/addr" merlin_winid) in
 	let (dataconn, datafid, dataio) = get_fullio O9pc.oWRITE (Printf.sprintf "%s/data" merlin_winid) in
 	let (ctlconn, ctlfid, ctlio) = get_fullio O9pc.oWRITE (Printf.sprintf "%s/ctl" merlin_winid) in
-	let (_:int32) = O9pc.write addrconn addrfid addrio 0L 1l "," in
-	let (_:int32) = O9pc.write dataconn datafid dataio 0L 0l "" in
+	let (_:int32) = O9pc.write addrconn addrfid addrio "," in
+	let (_:int32) = O9pc.write dataconn datafid dataio "" in
 	List.iter (fun m ->
-			O9pc.write dataconn datafid dataio 0L (Int32.of_int (String.length m)) m
+			O9pc.write dataconn datafid dataio m
 			|> (ignore: int32 -> unit)
 		)
 		ms;
-	let (_:int32) = O9pc.write addrconn addrfid addrio 0L 1l "0" in
-	let (_:int32) = O9pc.write ctlconn ctlfid ctlio 0L 20l "clean\ndot=addr\nshow\n" in
+	let (_:int32) = O9pc.write addrconn addrfid addrio "0" in
+	let (_:int32) = O9pc.write ctlconn ctlfid ctlio "clean\ndot=addr\nshow\n" in
 	O9pc.clunk addrconn addrfid;
 	O9pc.clunk dataconn datafid;
 	O9pc.clunk ctlconn ctlfid;
@@ -131,7 +131,7 @@ let get_content () =
 	let b = Buffer.create 1024 in
 	let (fid, io) = get_io conn O9pc.oREAD (Printf.sprintf "%s/body" wid) in
 	let rec fill offset =
-		let r = O9pc.read conn fid io (Int64.of_int offset) 1024l in
+		let r = O9pc.read conn fid io ~offset:(Int64.of_int offset) 1024l in
 		if r = "" then
 			()
 		else (
@@ -190,19 +190,19 @@ let ident_under_point code offset =
 let get_addr winid = O9pc.(
 	(* open addr *)
 	let conn_addr = get_conn () in
-	let root_addr = attach conn_addr user "" in
-	let addr_fid = walk conn_addr root_addr true (Printf.sprintf "%s/addr" winid) in
+	let root_addr = attach conn_addr ~user  "" in
+	let addr_fid = walk conn_addr root_addr ~reuse (Printf.sprintf "%s/addr" winid) in
 	let addr_iounit = fopen conn_addr addr_fid oREAD in
 
 	(* open ctl *)
 	let conn_ctl = get_conn () in
-	let root_ctl = attach conn_ctl user "" in
-	let ctl_fid = walk conn_ctl root_ctl true (Printf.sprintf "%s/ctl" winid) in
+	let root_ctl = attach conn_ctl ~user  "" in
+	let ctl_fid = walk conn_ctl root_ctl ~reuse (Printf.sprintf "%s/ctl" winid) in
 	let ctl_iounit = fopen conn_ctl ctl_fid oWRITE in
 
 	(* write to ctl and read from addr *)
-	let (_:int32) = write conn_ctl ctl_fid ctl_iounit 0L 9l "addr=dot\n" in
-	let s = read conn_addr addr_fid addr_iounit 0L 1024l in
+	let (_:int32) = write conn_ctl ctl_fid ctl_iounit "addr=dot\n" in
+	let s = read conn_addr addr_fid addr_iounit 1024l in
 	
 	(* clean up *)
 	clunk conn_addr addr_fid;
--- mkfile
+++ mkfile
@@ -1,14 +1,14 @@
 
 TRGTS=M/Cplt M/TypeOf M/Locate M/Errors
-LIBS=fcall.cmo o9pc.cmo LibAcme.cmo LibMerlin.cmo
+LIBS=LibAcme.cmo LibMerlin.cmo
 
 all:V: $TRGTS
 
 M/%: %.ml $LIBS
-	ocamlfind ocamlc -package unix,str,batteries,yojson -linkpkg -o $target $LIBS $stem.ml
+	ocamlfind ocamlc -package unix,str,batteries,yojson,o9p -linkpkg -o $target $LIBS $stem.ml
 
 %.cmo: %.ml
-	ocamlfind ocamlc -package unix,str,batteries,yojson -c -o $target $prereq
+	ocamlfind ocamlc -package unix,str,batteries,yojson,o9p -c -o $target $prereq
 
 clean:V:
 	rm -f *.cm? *.o *.a
@@ -23,15 +23,12 @@ Cplt.cmo : LibMerlin.cmo LibAcme.cmo
 Cplt.cmx : LibMerlin.cmx LibAcme.cmx
 Errors.cmo : LibMerlin.cmo LibAcme.cmo
 Errors.cmx : LibMerlin.cmx LibAcme.cmx
-LibAcme.cmo : o9pc.cmo fcall.cmo
-LibAcme.cmx : o9pc.cmx fcall.cmx
+LibAcme.cmo :
+LibAcme.cmx :
 LibMerlin.cmo :
 LibMerlin.cmx :
 Locate.cmo : LibMerlin.cmo LibAcme.cmo
 Locate.cmx : LibMerlin.cmx LibAcme.cmx
 TypeOf.cmo : LibMerlin.cmo LibAcme.cmo
 TypeOf.cmx : LibMerlin.cmx LibAcme.cmx
-fcall.cmo :
-fcall.cmx :
-o9pc.cmo : fcall.cmo
-o9pc.cmx : fcall.cmx
+
